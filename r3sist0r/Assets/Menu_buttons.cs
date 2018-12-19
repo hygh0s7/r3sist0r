ed77e57ba3c3095a195a97138e98647f68a56648
justine <Justineginchereau@gmail.com>
2018-12-19 02:31:42 -0500

    Updated main and created character menu base

---
+++ r3sist0r/Assets/Menu_buttons.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_buttons : MonoBehaviour {
+public GameObject MenuPanel;
+public GameObject CharacterPanel;
+	// Use this for initialization
+	void Start () {
+		 MenuPanel.SetActive(true);
+         CharacterPanel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void ShowCharacterPanel()
+     {
+         MenuPanel.SetActive(false);
+         CharacterPanel.SetActive(true);
+     }
+ 
+ public void ShowMenuPanel()
+     {
+         MenuPanel.SetActive(true);
+         CharacterPanel.SetActive(false);
+     }
+}
